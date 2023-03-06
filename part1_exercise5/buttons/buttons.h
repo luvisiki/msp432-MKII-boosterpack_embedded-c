c6c31ef88b9b1e35efccdd2e074727a9a9f61525
luvisiki_99) <65345064+luvisiki@users.noreply.github.com>
2023-03-06 17:05:14 +0800

    upload exercise5

---
+++ part1_exercise5/buttons/buttons.h
@@ -0,0 +1,30 @@
+/*
+ * buttons.h
+ *
+ *  Created on: 2023年3月1日
+ *      Author: luvisiki
+ */
+
+#ifndef BUTTONS_BUTTONS_H_
+#define BUTTONS_BUTTONS_H_
+
+#include "msp.h"
+#include "msp432p401r.h"
+#include <stdint.h>
+
+static uint8_t button_bottom_flag;  //global valueable.exercise4: change into static
+static uint8_t button_upper_flag;
+static uint8_t button_joystick_flag;
+
+void init_buttons (void);
+uint8_t read_button_bottom (void);
+uint8_t button_bottom_fsm(void);
+void init_buttons_int(void);
+uint8_t check_and_clear_bb_flag (void); //button_bottom
+uint8_t check_and_clear_bu_flag (void); //button_upper
+uint8_t check_and_clear_bj_flag (void); //button_joystick
+
+#endif /* BUTTONS_BUTTONS_H_ */
+
+
+
